b6baa0b0ffcf43291995ae7c72a7882eacde578f
kamillasecki <kamil.lasecki@eircom.net>
2015-11-14 20:18:39 +0000

    rubby added,  bye bye node  :/

--- node.js
+++ node.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var app =  express();
 
+
 app.use(express.static(__dirname + '/public'));
+
+app.get('/manager/:name' , function(req, res) {
+ res.setHeader('Content-type','text/plain');
+ res.send("You wanted to display :" + req.params.name);
+ 
+});
 
 var server = app.listen(process.env.PORT);
